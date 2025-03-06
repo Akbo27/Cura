cf6ebdda49ea448ff321e3e6a31fac4e1f172d45
Akbo27 <aungkaungbo.edu@gmail.com>
2025-03-06 15:51:11 +0700

    FEATURE ✨ Create Hospital Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Hospital;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\HospitalFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Hospital::factory(10)->create();
+        User::factory(10)->create();
     }
 }

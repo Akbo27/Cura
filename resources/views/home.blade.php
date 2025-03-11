29577213bd906bcbb99f3aaef363697f60336121
Akbo27 <aungkaungbo.edu@gmail.com>
2025-03-11 14:20:48 +0700

    DESIGN 🎨 New Home Blade Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,13 @@
-@extends('layouts.app')
+<x-site-layout>
+    @section('title', 'Welcome')
 
-@section('title', 'Welcome')
-
-@section('content')
-    <div class="text-center">
-        <h1 class="text-3xl font-bold">Welcome to Hospital Booking System</h1>
-        <p class="mt-2">Book an appointment with top doctors in your city.</p>
-        <a href="{{ route('appointments.create') }}" class="bg-blue-500 text-white px-6 py-3 mt-4 inline-block rounded">
-            Book Appointment Now
-        </a>
-    </div>
-@endsection
-
+    @section('content')
+        <div class="text-center">
+            <h1 class="text-8xl font-bold">Welcome to your path to wellness! </h1>
+            <p class="mt-2">Experience personalized and secure healthcare in Bangkok with CURA</p>
+            <a href="{{ route('appointments.create') }}" class="bg-blue-500 text-white px-6 py-3 mt-4 inline-block rounded">
+                Book Appointment Now
+            </a>
+        </div>
+    @endsection
+</x-site-layout>

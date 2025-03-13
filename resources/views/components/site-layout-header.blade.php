13402f10df145a33aad7eebbc62aff6ea986d351
Akbo27 <aungkaungbo.edu@gmail.com>
2025-03-13 17:36:27 +0700

    REFACTOR 🔨 Make some adjustments in components

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,9 +1,9 @@
-<nav class="bg-blue-500 text-white p-4">
+<nav class="bg-blue-600 text-white p-4">
     <div class="container mx-auto flex justify-between">
-        <a href="{{ route('home') }}" class="text-lg font-bold">Cura</a>
+        <a href="{{ route('home') }}" class="text-white text-lg font-bold">Cura</a>
         <div>
-            <a href="{{ route('appointments.index') }}" class="mr-4">My Appointments</a>
-            <a href="{{ route('appointments.specializations') }}" class="mr-4">Specializations</a>
+            <a href="{{ route('appointments.index') }}" class=" text-white mr-4">My Appointments</a>
+            <a href="{{ route('appointments.specializations') }}" class="text-white mr-4">Specializations</a>
 
             @auth
                 <form action="{{ route('logout') }}" method="POST" class="inline">

30f5ec454842709997a456efa4327682eb286cc3
Akbo27 <aungkaungbo.edu@gmail.com>
2025-03-14 00:20:35 +0700

    REFACTOR 🔨 Route changes fix

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\HospitalController;
 
+
 Route::get('/', function () {
     return view('home');
 })->name('home');
@@ -13,24 +14,29 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+// **Step 1: Choose Specialization (Available for everyone)**
+Route::get('/appointments/specializations', [AppointmentController::class, 'selectSpecialization'])
+    ->name('appointments.specializations');
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
-    // **Step 1: Choose specialization**
-    Route::get('/appointments/specializations', [AppointmentController::class, 'selectSpecialization'])
-        ->name('appointments.specializations');
+    // **Step 2: Show hospitals for the selected specialization**
+    Route::get('/appointments/hospitals/{specialization}', [AppointmentController::class, 'showHospitals'])
+        ->name('appointments.hospitals');
 
-    // **Step 2: Choose doctor, date & time**
-    Route::get('/appointments/book', [AppointmentController::class, 'showDoctorSelection'])
+    // **Step 3: Show available doctors in a selected hospital**
+    Route::get('/appointments/book/{hospital_id}', [AppointmentController::class, 'showDoctorSelection'])
         ->name('appointments.book');
 
+    // **Step 4: Store appointment**
     Route::post('/appointments/store', [AppointmentController::class, 'store'])
         ->name('appointments.store');
 
-    // **Resource routes for appointments (except create and store)**
+    // **Appointments list for the logged-in user**
     Route::resource('appointments', AppointmentController::class)->except(['create', 'store']);
 
-    // **Doctors & Hospitals**
+    // **Doctors & Hospitals Management**
     Route::resource('doctors', DoctorController::class);
     Route::resource('hospitals', HospitalController::class);
 });
